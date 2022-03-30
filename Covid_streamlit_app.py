57efcd8dd6db5e8233c19925bb30450be10e6b46
franciskingk <franciskingk@gmail.com>
2022-03-30 16:26:39 +0300

    Update Covid_streamlit_app.py

--- Covid_streamlit_app.py
+++ Covid_streamlit_app.py
@@ -32,7 +32,7 @@ with dataset:
 	df = pd.read_csv('/home/francis/Downloads/owid-covid-data.csv',parse_dates=['date'])
 	df.drop(['new_cases_smoothed','iso_code','icu_patients_per_million',], axis=1, inplace=True)
 	Kenya = df[df['location'] == 'Kenya']
-	Kenya.head()
+	preview=Kenya.tail()
 	st.table(preview)
  
 #
@@ -45,15 +45,15 @@ with visuals:
 
 	st.header('Trend on total Covid cases in Kenya from February-2020 to March-2022')
 	# time series plot for trend of post operative stays between 2019 and 2020 
-	plt.figure(figsize=(15,8))
+	fig, ax = plt.subplots()
 	sns.set_style("darkgrid")
 	sns.lineplot(x='date',y='total_cases',data=Kenya, hue='location').set(title='Trend on total Covid cases in Kenya from February-2020 to March-2022')
 	plt.show()
-	st.pyplot(figure)
+	st.pyplot(fig)
 
 	# Bar plot to check  the frequency of total hospital days
 	st.header('Trend on new Covid19 cases upto 24th March 2022')
-	plt.figure(figsize=(15,8))
+	fig, ax = plt.subplots()
 	sns.set_style("darkgrid")
 	sns.lineplot(x='date',y='new_cases',data= Kenya,hue='location').set(title="Trend on new Covid19 cases upto 24th March 2022")
 
@@ -62,7 +62,7 @@ with visuals:
 	# plotting to see the trend of people who stayed long in the hospital and whether they were alive or dead at the end
 
 	st.header("Trend on deaths due to Covid19 up to the 24th of March 2022")
-	plt.figure(figsize=(15,8))
+	fig, ax = plt.subplots()
 	sns.set_style("darkgrid")
 	sns.lineplot(x='date',y='new_deaths',hue='location',data=Kenya).set(title="Trend on deaths due to Covid19 up to the 24th of March 2022")
 	st.plotly_chart(fig)
@@ -71,23 +71,26 @@ with visuals:
 
 	st.header('Trend on Testing_rate for Covid19 up to the 24th of March 2022')
 	# plotting for the testing rate
-	plt.figure(figsize=(15,8))
+	fig, ax = plt.subplots()
 	sns.set_style("darkgrid")
 	sns.lineplot(x='date',y='new_tests',hue='location',data=Kenya).set(title="Trend on Testing_rate for Covid19 up to the 24th of March 2022")
 	st.plotly_chart(fig)
 
 	st.header('Trend on Positive rate for Covid19 up to the 24th of March 2022')
 	# plotting for the testing rate
-	plt.figure(figsize=(15,8))
+	fig, ax = plt.subplots()
 	sns.set_style("darkgrid")
 	sns.lineplot(x='date',y='positive_rate',hue='location',data=Kenya).set(title="Trend on Positive rate for Covid19 up to the 24th of March 2022")
+	st.plotly_chart(fig)
 
 	st.header('Trend on new vaccinations against Covid19 up to the 24th of March 2022')
 	# plotting for the testing rate
-	plt.figure(figsize=(15,8))
+	fig, ax = plt.subplots()
 	sns.set_style("darkgrid")
 	sns.lineplot(x='date',y='new_vaccinations',hue='location',data=Kenya).set(title="Trend on new vaccinations against Covid19 up to the 24th of March 2022")
+	st.plotly_chart(fig)
 
 	st.header('Timeline of People vaccinated vs those fully Vaccinated in Kenya upto 24th March 2022')
 	# plotting the people vaccinated vs people fully vaccinated
 	Kenya[['people_fully_vaccinated','people_vaccinated']].plot(figsize=(15,8),title='Timeline of People vaccinated vs those fully Vaccinated in Kenya upto 24th March 2022')
+	st.plotly_chart(fig)
